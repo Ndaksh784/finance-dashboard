09ef41f6d3b23577fd688ad3137203d760ffdb6d
Ndaksh784 <152335040+Ndaksh784@users.noreply.github.com>
2026-04-02 23:37:03 +0530

    Fixed LegDot file resolution issue

--- src/components/Common/index.js
+++ src/components/Common/index.js
@@ -1,5 +1,5 @@
 export { default as StatCard }          from "./StatCard";
-export { default as LegDot }            from "./LegDot";
+export { default as LegDot } from "./LegDot.jsx";
 export { ChartTooltip, PieTooltip }     from "./ChartTooltip";
 export { default as InsCard }           from "./InsCard";
 export { default as SortBtn }           from "./SortBtn";
